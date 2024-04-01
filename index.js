8a952b2b0554a4e094f3ebb6139b5aca33993bc9
rasheeda <raskieda@gmail.com>
2024-04-01 10:22:23 +0200

    added eventListener to close modal

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.textContent = `
       <h3>${studentName}</h3>
     `;
     
@@ -39,8 +39,8 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
-      
+    closeModal.addEventListener('click', function () {
+      modal.style.display = 'none';
     });
   });
   
